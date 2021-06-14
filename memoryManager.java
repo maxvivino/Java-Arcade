8620bd51212efb51a4dc7a17a042af2224e57857
maxvivino <60784370+maxvivino@users.noreply.github.com>
2021-06-14 13:24:25 -0400

    Changes

--- memoryManager.java
+++ memoryManager.java
@@ -1,44 +1,54 @@
-import java.util.Scanner;
-
-public class memoryManager {
-    //static int numbers;
-    static boolean alive;
-    static int money;
-    static String play;
-    static int bet;
-    static int[] numbers;
-    static int x;
-    static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        money = 100;
-        play = "y";
-        int[] numbers = new int[x];
-        memoryManager gerald = new memoryManager();
-        gerald.memManager();
-    }
-    //public memoryManager() {
-
-    //}
-    public static void memManager() {
-        System.out.println("\nWelcome to Marcelo's Memory Mountain, how much would you like to bet.");
-        bet = scan.nextInt();
-        while (bet > money || bet <= 0) {
-            while (bet > money) {
-                System.out.println("You need to bet money you have you dummy.");
-                bet = scan.nextInt();
-            }
-            while (bet <= 0) {
-                System.out.println("You need to bet money.");
-                bet = scan.nextInt();
-            }
-        }
-        alive = true;
-        System.out.println("You choose to bet " + bet + " Here is the first sequence");
-        //make a for loop starting a 3 increasing everytime
-            numbers = (int) (Math.random() * 6 + 1);
-            System.out.println(numbers);
-        }
-
-    }
-
-}
+//import java.util.Scanner;
+//
+//public class memoryManager {
+//    //static int numbers;
+//    static boolean alive;
+//    static int money;
+//    static String play;
+//    static int bet;
+//    static int[] numbers;
+//    static int x;
+//    static Scanner scan = new Scanner(System.in);
+//    public static void main(String[] args) {
+//        money = 100;
+//        play = "y";
+//        int[] numbers = new int[x];
+//        memoryManager gerald = new memoryManager();
+//        gerald.memManager();
+//
+//    }
+//
+//
+//
+//
+//    public static void memManager() {
+//        System.out.println("\nWelcome to Marcelo's Memory Mountain, how much would you like to bet.");
+//        bet = scan.nextInt();
+//        while (bet > money || bet <= 0) {
+//            while (bet > money) {
+//                System.out.println("You need to bet money you have you dummy.");
+//                bet = scan.nextInt();
+//            }
+//            while (bet <= 0) {
+//                System.out.println("You need to bet money.");
+//                bet = scan.nextInt();
+//            }
+//        }
+//        alive = true;
+//        System.out.println("You choose to bet " + bet + " Here is the first sequence");
+//        //make a for loop starting a 3 increasing everytime
+//           // numbers = ;
+//
+//            System.out.println((int) (Math.random() * 6 + 1));
+//            System.out.println((int) (Math.random() * 6 + 1));
+//            System.out.println((int) (Math.random() * 6 + 1));
+//            System.out.println("Repeat the numbers");
+//
+//
+//            }
+//        }
+//
+//
+//
+//
+//
